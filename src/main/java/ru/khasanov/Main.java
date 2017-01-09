ad32c92e3f1d850a5f2fa50edfe37cb513e74f3f
Bulat Khasanov <afti@yandex.ru>
2017-01-09 06:03:51 +0300

    Добавил log4j2 и настройку логирования

--- src/main/java/ru/khasanov/Main.java
+++ src/main/java/ru/khasanov/Main.java
@@ -2,17 +2,33 @@ package ru.khasanov;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import ru.khasanov.settings.Settings;
 
 /**
  * Created by bulat on 05.01.17.
  */
 public class Main {
+    private static void disableLogging() {
+        LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
+        Configuration config = loggerContext.getConfiguration();
+        LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
+        loggerConfig.setLevel(Level.OFF);
+        loggerContext.updateLoggers();
+    }
+
     public static void main(String[] args) {
         Settings settings = new Settings();
         JCommander jCommander = new JCommander(settings);
         try {
             jCommander.parse(args);
+            if (!settings.isLoggingEnable()) {
+                disableLogging();
+            }
             Server server = new Server(settings);
             server.start();
         } catch (ParameterException e) {
